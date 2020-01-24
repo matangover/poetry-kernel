803dbee36be500adbfc287eaf35e2753bec63b4e
Matan Gover <matangover@gmail.com>
2020-01-24 00:12:02 -0500

    Improve printed text.

--- new-notebook-project.py
+++ new-notebook-project.py
@@ -5,7 +5,7 @@ import sys
 from subprocess import check_call
 
 if len(sys.argv) != 2 or sys.argv[1] in ('--help', '-h'):
-    print('Usage: %s <project-path>' % sys.argv[0])
+    print('Usage: %s <project-directory>' % sys.argv[0])
     sys.exit(1)
 
 project_path = sys.argv[1]
@@ -19,17 +19,14 @@ notebook_template = os.path.join(os.path.dirname(__file__), 'python-poetry.ipynb
 notebook = os.path.join(project_path, project_name + '.ipynb')
 shutil.copyfile(notebook_template, notebook)
 
-print(('''
+print('''
 Done. Notebook created: {0}
 
-To add dependencies, run `poetry add <package>` in the project directory ({1}/),
-or directly from the notebook: `!poetry add <package>`.
+Open this notebook in Jupyter to start working. Additional notebooks created
+inside the project directory will run in the same virtual environment, as long
+as you use the Python (Poetry) kernel.
 
-Dependencies are tracked in {1}/pyproject.toml.
-When running the notebook on another machine, ensure all dependencies are
-installed by running `poetry install`.
-
-Any additional notebooks created in the project directory will share the same
-virtual environment, as long as you use the Python (Poetry) kernel.'''
-    .format(notebook, project_path)
-))
+To add dependencies, run `poetry add <package>` in the project directory or
+directly from a notebook: `!poetry add <package>`.\
+'''.format(notebook)
+)
