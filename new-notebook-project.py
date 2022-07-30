a0ad373a08df8a9354370ab808db0bc39575f719
Matan Gover <matangover@gmail.com>
2022-07-30 22:11:47 +0300

    Fix issue when running inside activated virtualenv
    https://github.com/matangover/poetry-kernel/issues/2

--- new-notebook-project.py
+++ new-notebook-project.py
@@ -12,8 +12,16 @@ project_path = sys.argv[1]
 project_name = os.path.basename(project_path)
 print('Creating project: ' + project_name)
 os.mkdir(project_path)
-check_call(['poetry', 'init', '-q', '-n'], cwd=project_path)
-check_call(['poetry', 'add', 'ipykernel'], cwd=project_path)
+# Force poetry to ignore any active venv by removing the VIRTUAL_ENV and CONDA_PREFIX env vars.
+# See:
+# https://github.com/python-poetry/poetry/issues/281#issuecomment-403102022
+# https://github.com/python-poetry/poetry/blob/e0bc7f3fc06a6f1caca694432c9f8bd8c0af30db/src/poetry/utils/env.py#L679
+# https://github.com/python-poetry/poetry/issues/4622
+env = os.environ.copy()
+env.pop('VIRTUAL_ENV', None)
+env.pop('CONDA_PREFIX', None)
+check_call(['poetry', 'init', '-q', '-n'], cwd=project_path, env=env)
+check_call(['poetry', 'add', 'ipykernel'], cwd=project_path, env=env)
 
 notebook_template = os.path.join(os.path.dirname(__file__), 'python-poetry.ipynb.template')
 notebook = os.path.join(project_path, project_name + '.ipynb')
